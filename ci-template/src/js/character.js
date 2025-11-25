0cbe63b12aca514da8bf1d05e67d6bcc2b181e1f
Maksim <Mikimiki89@yandex.ru>
2025-11-25 23:18:47 +0300

    2

--- ci-template/src/js/character.js
+++ ci-template/src/js/character.js
@@ -1,13 +1,13 @@
 export class Character {
     constructor(name, type){
         if(name.length < 2 ||  name.length > 10){                                         // имя война
-            console.error('throw new Error(...)');
+            console.Error('throw new Error(...)');
         }else this.name = name;
         
         const warmans = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'] // массив войнов
         if(warmans.includes(type)){                                                       // тип война
             this.type = type;
-        } else {console.error('throw new Error(...)');
+        } else {console.Error('throw new Error(...)');
             }
 
         this.health = 100;                                                                // уровень жизни
